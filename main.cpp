e834d70a67e858b0766eb90d8acff84432fc13dd
kaname111 <zanzou1209@gmail.com>
2026-04-28 10:53:29 +0900

    三角形の問題の追加

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <vector>
 #include <random>
 using namespace std;
 
@@ -12,7 +13,7 @@ struct Question
 
 int main()
 {
-	Question question[3];
+	vector<Question> questions(3);
 
 	random_device rd;
 	mt19937 rand(rd());
@@ -20,28 +21,35 @@ int main()
 	//掛け算
 	int x = uniform_int_distribution<>(1, 30)(rand);
 	int y = uniform_int_distribution<>(1, 20)(rand);
-	question[0].q = to_string(x) + "x" + to_string(y);
-	question[0].a = x * y;
+	questions[0].q = to_string(x) + "x" + to_string(y);
+	questions[0].a = x * y;
 
 	//割り算
 	x = uniform_int_distribution<>(1, 30)(rand);
 	y = uniform_int_distribution<>(1, 20)(rand);
-	question[1].q = to_string(x * y) + "÷" + to_string(y);
-	question[1].a = x;
+	questions[1].q = to_string(x * y) + "÷" + to_string(y);
+	questions[1].a = x;
 
 	//複雑な式
 	x = uniform_int_distribution<>(1, 100)(rand);
 	y = uniform_int_distribution<>(1, 10)(rand);
 	int z = uniform_int_distribution<>(1, 10)(rand);
 	int w = uniform_int_distribution<>(1, 10)(rand);
-	question[2].q =
+	questions[2].q =
 		to_string(x) + "-(" + to_string(y * w) + "+" + to_string(z * w) + ")÷" +
 		to_string(w);
-	question[2].a = x - (y + z);
+	questions[2].a = x - (y + z);
+
+	//三角形の面積
+	x = uniform_int_distribution<>(1, 10)(rand);
+	y = uniform_int_distribution<>(1, 5)(rand) * 2;
+	questions.push_back({
+		"面積" + to_string(x * y / 2) + "cm^2、底辺" + to_string(y) + "cmの三角形の高さを求めよ。",
+		x });
 
 	cout << "[リクルート試験対策クイズ]\n";
 
-	for (const auto& e : question)
+	for (const auto& e : questions)
 	{
 		cout << e.q << "の答えは？\n";
 
