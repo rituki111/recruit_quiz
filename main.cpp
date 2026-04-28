358b52cbaf5153ddc1e6411dbb04115f6fa49c42
kaname111 <zanzou1209@gmail.com>
2026-04-28 10:58:01 +0900

    問題文の末尾を自然な文にする

--- main.cpp
+++ main.cpp
@@ -21,13 +21,13 @@ int main()
 	//Š|‚¯Z
 	int x = uniform_int_distribution<>(1, 30)(rand);
 	int y = uniform_int_distribution<>(1, 20)(rand);
-	questions[0].q = to_string(x) + "x" + to_string(y);
+	questions[0].q = to_string(x) + "x" + to_string(y) + "‚Ì“š‚¦‚Í?";
 	questions[0].a = x * y;
 
 	//Š„‚èZ
 	x = uniform_int_distribution<>(1, 30)(rand);
 	y = uniform_int_distribution<>(1, 20)(rand);
-	questions[1].q = to_string(x * y) + "€" + to_string(y);
+	questions[1].q = to_string(x * y) + "€" + to_string(y) + "‚Ì“š‚¦‚Í?";
 	questions[1].a = x;
 
 	//•¡G‚È®
@@ -37,7 +37,7 @@ int main()
 	int w = uniform_int_distribution<>(1, 10)(rand);
 	questions[2].q =
 		to_string(x) + "-(" + to_string(y * w) + "+" + to_string(z * w) + ")€" +
-		to_string(w);
+		to_string(w) + "‚Ì“š‚¦‚Í?";
 	questions[2].a = x - (y + z);
 
 	//OŠpŒ`‚Ì–ÊÏ
@@ -51,7 +51,7 @@ int main()
 
 	for (const auto& e : questions)
 	{
-		cout << e.q << "‚Ì“š‚¦‚ÍH\n";
+		cout << e.q << "\n";
 
 		int answer;
 		cin >> answer;
