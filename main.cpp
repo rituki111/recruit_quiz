c3871c4f2acebd81f9c3c0107217c27d888b8479
kaname111 <zanzou1209@gmail.com>
2026-04-28 11:50:12 +0900

    分数や文字を処理できるように答えの型をintからsting型に変更

--- main.cpp
+++ main.cpp
@@ -7,8 +7,8 @@ using namespace std;
 //問題文と答えをまとめる構造体
 struct Question
 {
-	string q;  //問題文
-	int a;     //答え
+	string q; //問題文
+	string a; //答え
 };
 
 int main()
@@ -22,13 +22,13 @@ int main()
 	int x = uniform_int_distribution<>(1, 30)(rand);
 	int y = uniform_int_distribution<>(1, 20)(rand);
 	questions[0].q = to_string(x) + "x" + to_string(y) + "の答えは?";
-	questions[0].a = x * y;
+	questions[0].a = to_string(x * y);
 
 	//割り算
 	x = uniform_int_distribution<>(1, 30)(rand);
 	y = uniform_int_distribution<>(1, 20)(rand);
 	questions[1].q = to_string(x * y) + "÷" + to_string(y) + "の答えは?";
-	questions[1].a = x;
+	questions[1].a = to_string(x);
 
 	//複雑な式
 	x = uniform_int_distribution<>(1, 100)(rand);
@@ -38,14 +38,14 @@ int main()
 	questions[2].q =
 		to_string(x) + "-(" + to_string(y * w) + "+" + to_string(z * w) + ")÷" +
 		to_string(w) + "の答えは?";
-	questions[2].a = x - (y + z);
+	questions[2].a = to_string(x - (y + z));
 
 	//三角形の面積
 	x = uniform_int_distribution<>(1, 10)(rand);
 	y = uniform_int_distribution<>(1, 5)(rand) * 2;
 	questions.push_back({
 		"面積" + to_string(x * y / 2) + "cm^2、底辺" + to_string(y) + "cmの三角形の高さを求めよ。",
-		x });
+		to_string(x) });
 
 	//円錐の面積
 	x = uniform_int_distribution<>(1, 10)(rand);
@@ -53,14 +53,14 @@ int main()
 	questions.push_back({
 		"底辺の半径" + to_string(x) + "cm、高さ" + to_string(y) + "cmの円錐がある。\n" +
 		"この円錐の体積をXπcm^3とする。Xの値を求めよ。",
-		x * x * y / 3 });
+		to_string(x * x * y / 3) });
 
 	//球の体積
 	x = uniform_int_distribution<>(1, 5)(rand) * 3;
 	questions.push_back({
 		"球の半径" + to_string(x) + "cmの球がある。\n" +
 		"この球の体積をXπcm^3とする。Xの値を求めよ。",
-		x * x * x * 4 / 3});
+		to_string(x * x * x * 4 / 3)});
 
 	cout << "[リクルート試験対策クイズ]\n";
 
@@ -68,7 +68,7 @@ int main()
 	{
 		cout << e.q << "\n";
 
-		int answer;
+		string answer;
 		cin >> answer;
 		if (answer == e.a)
 		{
