3153c76566d7544fbf7b6718250f1df46d2ccff8
kaname111 <zanzou1209@gmail.com>
2026-07-07 12:12:15 +0900

    振り子の問題を追加した。

--- exam_science.cpp
+++ exam_science.cpp
@@ -223,6 +223,17 @@ QuestionList CreatePhysicsExam()
 			"kgの重りをつけ、わずかに揺らしたところ、周期はX秒であった。Xの値を求めよ。\n" +
 			"なお、重力加速度gと円周率πについて、π = √gが成り立つものとする。",
 			to_string(2 * l) });
+
+		l = uniform_int_distribution<>(1, 10)(rd);  //長さ(m)
+		m = uniform_int_distribution<>(1, 10)(rd);  //質量(kg)
+		int max_v = (int)sqrt(20 * l);  //速度の上限(2gh = v^2を根拠とする)
+		int v = uniform_int_distribution<>(1, max_v)(rd);  //速度
+		questions.push_back({
+			"重力速度を10m/s^2とする。\n長さ" + to_string(l) + "mの糸に質量" + to_string(m) +
+			"kgの重りをつけた振り子がある。\nこの重りを最下点から高さ" +
+			to_string(v * v * 100 / 20) + "cmの位置で静かに放した。\n" +
+			"このとき、重りが最下点を通過する時の速度をm/s単位で求めよ。",
+			to_string(v) });
 	}  //振り子
 
 	return questions;
